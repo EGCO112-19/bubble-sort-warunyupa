4c9d41e9c9e238f3c031ff86306e3abd1c251015
warunyupa <warunyupa.ler@student.mahidol.ac.th>
2021-01-29 07:18:00 +0000

    edit file

--- simpleMain.c
+++ simpleMain.c
@@ -3,7 +3,15 @@
 
 void main(int argc, char **argv){
   printf("There are %d arguments\n",argc-1);
-  int i;
+  int i,N;
+  N = argc-1;
+
+  int *a;
+  a = (int *)malloc(sizeof(int)*N);
+
   for(i=1;i<argc;i++)
     printf("argument#%d is %d\n",i, atoi(argv[i]));
 }
+
+bubble(a,N);
+return 0;
